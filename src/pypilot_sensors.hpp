e5ebfa3f7c93da4c91d7f7bf3760697cc00983f9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 16:55:02 -0400

    Add public umbrella header

---
+++ src/pypilot_sensors.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "pypilot_sensors/samples.hpp"
+#include "pypilot_sensors/data_model_writer.hpp"
+#include "pypilot_sensors/sensors_manager.hpp"
+#include "pypilot_sensors/adapters/nmea0183_sensor_adapter.hpp"
+#include "pypilot_sensors/adapters/signalk_sensor_adapter.hpp"
+#include "pypilot_sensors/adapters/servo_rudder_sensor_adapter.hpp"
+#include "pypilot_sensors/drivers/replay_sensor_adapter.hpp"
