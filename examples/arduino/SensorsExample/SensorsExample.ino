dd3938b38d55a8ff209c8eb0db5f043b9dead865
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 17:00:06 -0400

    Add Arduino sensors example

---
+++ examples/arduino/SensorsExample/SensorsExample.ino
@@ -0,0 +1,17 @@
+#include <pypilot_sensors.hpp>
+
+pypilot_data_model::DataModel<float> model;
+pypilot_sensors::SensorDataModelWriter<float> writer;
+
+void setup() {
+  Serial.begin(115200);
+  pypilot_sensors::GpsSample<float> gps;
+  gps.time_us = 1000;
+  gps.source = pypilot_data_model::SensorSource::serial;
+  gps.speed_valid = true;
+  gps.speed_kn = 4.0f;
+  writer.write_gps(model, gps);
+  Serial.println(model.navigation.gps.speed_kn.value);
+}
+
+void loop() {}
